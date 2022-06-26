c4c0674babf51f85ac2eecdc55010c3bf3b92832
Nopanun Laochunhanun (Thaen) <nopanun@central.tech>
2022-06-27 04:02:22 +0700

    Create routes, and add them to the router

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-const name = ;
+require('dotenv').config();
+
+const expres = require('express');
+const workoutRoutes = require('./routes/workout');
+
+// express app
+const app = expres();
+
+app.use((req, res, next) => {
+    console.log(req.method, req.path);
+    next();
+}).use(expres.static('public'));
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+// listen on port
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port', process.env.PORT);
+});
