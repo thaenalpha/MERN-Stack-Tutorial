200405e5993b6f2fc2e33ff3e45487836ce7d36c
Nopanun Laochunhanun (Thaen) <nopanun@central.tech>
2022-06-27 04:35:33 +0700

    Connect to MongoDB along with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const expres = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workout');
 
 // express app
@@ -9,12 +10,17 @@ const app = expres();
 app.use((req, res, next) => {
     console.log(req.method, req.path);
     next();
-}).use(expres.static('public'));
+}).use(expres.json());
 
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen on port
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port', process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+        .then(() => {
+            // listen on port
+            app.listen(process.env.PORT, () => {
+                console.log('Server is running on port', process.env.PORT);
+            });
+        })
+        .catch(err => console.error(err));
